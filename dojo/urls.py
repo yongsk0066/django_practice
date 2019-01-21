714f50b8370751d6cba0a9c1ee68945ebeb128cf
yongsk0066 <yongsk0066@naver.com>
2019-01-21 17:16:00 +0900

    2-dojo

---
+++ dojo/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+]
---
+++ dojo/views.py
@@ -0,0 +1,11 @@
+# dojo/views.py
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def mysum(request, numbers):
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    return HttpResponse(result)
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}씨. {}살이시네요.'.format(name, age))
