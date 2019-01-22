baa2f751b9d18846f0d4be7787ccd6e9cb8ed6e3
yongsk0066 <yongsk0066@naver.com>
2019-01-23 02:25:36 +0900

    lnglat 추가

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+import re
+from django.forms import ValidationError
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]\d+\.?\d*),([+-]\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해 주세요. 최대 100자 내외.')
     content = models.TextField(verbose_name='내용')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True,
+                              validators=[lnglat_validator], help_text='위도/경도 포맷으로 입력')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
