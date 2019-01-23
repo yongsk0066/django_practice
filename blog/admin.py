cced8ac2adce9f949ac347c90b8754f4b2d4c5e3
yongsk0066 <yongsk0066@naver.com>
2019-01-23 13:27:53 +0900

    status 추가

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,16 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id','title', 'content_size', 'created_at','updated_at']
+    list_display = ['id','title', 'content_size','status', 'created_at','updated_at']
+
+    actions = ['make_published']
 
     def content_size(self, post):
         return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     content_size.short_description = '글자수'
+
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{} sucessfully marked as published'.format(updated_count))
+    make_published.short_description = '지정 포스팅을 Published 상태로 변경합니다.'
--- blog/models.py
+++ blog/models.py
@@ -8,10 +8,17 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d','Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해 주세요. 최대 100자 내외.')
     content = models.TextField(verbose_name='내용')
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
                               validators=[lnglat_validator], help_text='위도/경도 포맷으로 입력')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
