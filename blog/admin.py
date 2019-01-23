886cc9e2fbefd59dd3b425f55093914442d6d791
yongsk0066 <yongsk0066@naver.com>
2019-01-23 12:56:53 +0900

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','title','created_at','updated_at']
--- first_django/settings.py
+++ first_django/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'first',
     'blog',
     'dojo',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
