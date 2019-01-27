65f3777cc2a513a493993f1aaa068e1c8b5f99b1
yongsk0066 <yongsk0066@naver.com>
2019-01-27 15:56:43 +0900

    django-debug-toolbar setup

--- first_django/settings.py
+++ first_django/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'first',
     'blog',
     'dojo',
+    'debug_toolbar',
     ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = ['127.0.0.1']
--- first_django/urls.py
+++ first_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path, re_path
@@ -24,3 +25,8 @@ urlpatterns = [
 ]
 
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
