2c228400f86592443acfe7a7e24f8e38220215f2
yongsk0066 <yongsk0066@naver.com>
2019-01-29 16:31:04 +0900

    blog.models.Post 모델에 get_absolute_url구현

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 import re
 from django.forms import ValidationError
+from django.urls import reverse
+
 
 def lnglat_validator(value):
     if not re.match(r'^([+-]\d+\.?\d*),([+-]\d+\.?\d*)$', value):
@@ -31,6 +33,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', arg=[self.id])
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,)
