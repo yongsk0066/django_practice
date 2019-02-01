c33fad9c6aa21013b1f2e99deb9417e16b8d7b9c
yongsk0066 <yongsk0066@naver.com>
2019-02-01 14:11:44 +0900

    MEDIA 세팅을 하고, blog.models.Post 모델에 photo 필드 추가

--- first_django/settings.py
+++ first_django/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'askdjango', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INTERNAL_IPS = ['127.0.0.1']
 
