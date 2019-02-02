9e67d1376edfda52a1589d3f80b7fb71b5a96d69
yongsk0066 <yongsk0066@naver.com>
2019-02-02 18:45:48 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('profile/', views.profile)
+    path('profile/', views.profile),
+    path('signup/', views.signup, name='signup')
 ]
