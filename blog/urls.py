72eb5724530173c4c3124b099189c233bbb73fe8
yongsk0066 <yongsk0066@naver.com>
2019-02-02 17:25:29 +0900

    CBV : DeleteView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('new/', views.post_new, name='post_new'),
     re_path(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
     re_path(r'^cbv/(?P<pk>\d+)/edit/$', views_cbv.post_edit),
+    re_path(r'^cbv/(?P<pk>\d+)/delete/$', views_cbv.post_delete),
 
 ]
 
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -20,3 +20,5 @@ post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 post_new = CreateView.as_view(model=Post)
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
