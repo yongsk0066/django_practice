fc590196ac9b63276a2ec2a624e5c60256394cd7
yongsk0066 <yongsk0066@naver.com>
2019-02-02 16:33:00 +0900

    CBV 샘플 - STEP 4. CBV 구현

--- dojo/views.py
+++ dojo/views.py
@@ -1,32 +1,13 @@
 # dojo/views.py
 from django.http import HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
-
+from django.views.generic import DetailView
 from dojo.forms import PostForm
 from dojo.models import Post
 
-class DetailView(object):
-    def __init__(self, model):
-        self.model = model
 
-    def get_object(self, *arg, **kwargs):
-        return get_object_or_404(self.model, id=kwargs['id'])
 
-    def get_template_name(self):
-        return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
-
-    def dispatch(self, request, *args, **kwargs):
-        return render(request, self.get_template_name(), {
-            self.model._meta.model_name: self.get_object(*args, **kwargs),
-        })
-    @classmethod
-    def as_view(cls, model):
-        def view(request, *args, **kwargs):
-            self = cls(model)
-            return self.dispatch(request, *args, **kwargs)
-        return view
-
-post_detail = DetailView.as_view(Post)
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
 def post_new(request):
     if request.method == 'POST':
