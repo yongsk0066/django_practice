9469409088eb65c39fe7a65a4b6cb753d74e60c0
yongsk0066 <yongsk0066@naver.com>
2019-02-02 20:28:42 +0900

    회원가입시에 이메일 추가 입력

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,20 @@
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
 
-
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
